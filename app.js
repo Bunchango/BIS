c8f0d4c7586da1f776142ad3a6b035adc29451fd
Bunchango <119033041+Bunchango@users.noreply.github.com>
2023-11-08 10:31:34 +0700

    add files

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+// Load global vars
+dotenv.config({path: "./config/config.env"})
+const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+// Connect to db
+connectDB();
+
+// Enable cors
+app.use(cors());
 
 // View engine
 app.set("view engine", "ejs");
 
-// Routes
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/", (req, res) => {
     res.render("index");
 })
 
-app.listen(3000, () => {
-    console.log("Listening on port 3000");
+// Set up routers
+
+
+app.listen(PORT, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 })
