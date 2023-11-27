e07a2798c59450e934c4e06abc25c85c178a69d1
Dong Manh Duc <s3978290@rmit.edu.vn>
2023-11-27 12:35:06 +0700

    created schemas

---
+++ models/book.js
@@ -0,0 +1,65 @@
+const mongoose = require("mongoose");
+
+const bookSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: [5, "Book name must be at least 5 characters"], 
+        maxlength: [24, "Book name must be at most 24 characters"]
+    },
+    images: {
+        type: [{
+            data: Buffer,
+            contentType: String,
+        }],
+        required: true,
+        validate: [imageLimit, "Images exceeds the limit of 3"],
+    },
+    author: {
+        type: String, 
+        required: true, 
+        minlength: [5, "Author name must be at least 5 characters"], 
+        maxlength: [24, "Author name must be at most 24 characters"]
+    }, 
+    tags: {
+        type: [{
+            type: String, 
+            enum: [
+                "Mystery", "Thriller", " Romance", "Biography", "Memoir", "Self-Help", "History", "Science", "Fantasy", "Sci-fi", 
+                "Horror", "Action", "Adventure", "Children's", "Comedy", "Poetry", "Philosophy", "Religion"
+            ]
+        }],
+        required: true, 
+    }, 
+    library: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Library",
+        required: true,
+    }, 
+    amount: { // Available books field will have the same initial value as amount
+        type: Number, 
+        required: true, 
+        default: 1,
+    },
+    dateImported: { // When add more books ( import more books ) update date imported field 
+        type: Date, 
+        default: Date.now, 
+        required: true,
+    },
+})
+
+// When user borrow book ( or schedule borrow ) reduce available books by 1
+// When user return book, increment available books by 1
+// When librarian add more book (import more book) increment both availableBooks and amount
+bookSchema.pre("save", function(next) {
+    if (this.isNew) {
+        this.available = this.amount;
+    }
+    next();
+})
+
+function imageLimit(val) {
+    return val.length <= 3;
+}
+
+module.exports = mongoose.model("Book", bookSchema);  
--- models/user.js
+++ models/user.js
@@ -1,2 +1,73 @@
 const mongoose = require("mongoose");
 
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String, 
+        unique: [true, "username already exists"],
+        required: [true, "username is required"], 
+        minlength: [5, "username must be at least 6 characters"],
+        maxlength: [12, "username must be at most 12 characters"],
+    }, 
+    password: {
+        type: String, 
+        required: [true, "password is required"],
+        validate: [passwordValidator, "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character"]
+    }, 
+    gmail: {
+        type: String, 
+        unique: [true, "email already exists"], 
+        required: [true, "gmail is required"]
+    }, 
+    profilePicture: {
+        data: Buffer,
+        contentType: String,
+    },
+})
+
+const readerSchema = new mongoose.Schema({
+    watchlist: [{
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "Book"
+    }], 
+    // When a book is new due date, or available of wishlist book is > 0, or librarian send message(rethink), or book is due, then create a new notification
+    notification: [{ 
+        type: String, 
+    }], 
+    joinedOn: {
+        type: Date, 
+        default: Date.now, 
+        required: true,
+    }
+})
+
+const librarianSchema = new mongoose.Schema({
+    library: {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "Library",
+        required: true
+    }
+})
+
+const adminSchema = new mongoose.Schema({
+    library: {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "Library",
+        required: true
+    }
+})
+
+function passwordValidator(val) {
+    return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(val);
+}
+
+const User = mongoose.model("User", userSchema);
+const Reader = User.discriminator("Reader", readerSchema);
+const Librarian = User.discriminator("Librarian", librarianSchema);
+const Admin = User.discriminator("Admin", adminSchema);
+
+module.exports = {
+  User,
+  Reader,
+  Librarian,
+  Admin,
+};
