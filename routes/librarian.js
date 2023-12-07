cf8312b1f49396239a4b8e2ba7e5125fa8ab6ccd
TurkeyMan <s3978290@rmit.edu.vn>
2023-12-07 17:24:38 +0700

    add comments

--- routes/librarian.js
+++ routes/librarian.js
@@ -1,3 +1,8 @@
 const router = require("express").Router();
 
+// Librarian can add, modify, delete books 
+// Librarian can check who is borrowing the books, who have not returned the books, who is going to come borrow the books
+// Librarian can confirm, or cancel, or mark schedule pickup as finished
+// Librarian can send messages to readers 
+
 module.exports = router;
