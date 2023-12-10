ebecf703f7ff8d0cef855daa3eb90a6844c29765
TurkeyMan <s3978290@rmit.edu.vn>
2023-12-10 20:21:42 +0700

    modify library schema

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const checkinRoutes = require('./routes/checkin');
 const readerRoutes = require('./routes/reader');
 const libraryRoutes = require('./routes/library');
 const librarianRoutes = require('./routes/librarian');
+const adminRoutes = require('./routes/admin');
 
 // Load global vars
 dotenv.config({ path: "./config/config.env" })
@@ -65,29 +66,31 @@ app.get("/", (req, res) => {
 
 app.use("/uploads", express.static("uploads"));
 
-// TODO: Recheck later when done main pages 
+// TODO: Change reader to redirect
 app.get("/homepage", (req, res) => {
     // If user has not login render reader's home page, if logged in render respective user's home page
     if (!req.user || req.user.__t === "Reader") {
         res.render("reader/homepage", { user: req.user, categories: categoriesArray });
     } else if (req.user && req.user.__t === "Librarian") {
-        res.render("librarian/library", { user: req.user });
+        res.redirect("/librarian/dashboard");
     } else if (req.user && req.user.__t === "Library") {
-        res.render("admin/library", { user: req.user });
+        res.redirect("/library/manage");
+    } else if (req.user && req.user.__t === "Admin") {
+        res.redirect("/admin/create_library");
     }
 })
 
 // Route for terms and conditions
-app.get("/terms", (req, res) => {
+app.get("/terms", (req, res) => { 
     res.render("reader/terms", { user: req.user });
 })
-
 
 // Set up routers
 app.use("/checkin", checkinRoutes);
 app.use("/reader", readerRoutes);
 app.use("/library", libraryRoutes);
 app.use("/librarian", librarianRoutes);
+app.use("/admin", adminRoutes);
 
 // Route for 404 page
 // TODO: Create a 404 page front end
@@ -95,6 +98,20 @@ app.get("*", (req, res) => {
     res.status(404).send("404 Page Not Found");
 })
 
+async function createAdmin() {
+    const {Admin} = require("./models/user");
+    const bcrypt = require("bcrypt");
+    const admin = new Admin({
+        username: "system_admin", 
+        password: await bcrypt.hash("system_admin", 10),
+        gmail: "system_admin",
+    })
+    await admin.save();
+    console.log("Created admin");
+}
+
+// createAdmin();
+ 
 app.listen(PORT, () => {
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 })
