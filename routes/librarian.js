689c5879d30422d96e7830c7b1a222c151ccbdcf
nguyenpnguyen <nguyen.pn@proton.me>
2023-12-12 22:51:05 +0700

    fix some random stuff

--- routes/librarian.js
+++ routes/librarian.js
@@ -1,7 +1,7 @@
 const Library = require("../models/library");
-const {Book, categoriesArray} = require("../models/book");
-const {Borrow, Pickup} = require("../models/borrow");
-const {validateBookCreation, validateUsername} = require("../config/validator");
+const { Book, categoriesArray } = require("../models/book");
+const { Borrow, Pickup } = require("../models/borrow");
+const { validateBookCreation, validateUsername } = require("../config/validator");
 const upload = require("./../config/multer");
 const { validationResult } = require("express-validator");
 const { response } = require("express");
@@ -28,10 +28,10 @@ function isLibrarian(req, res, next) {
 router.get("/profile", isLibrarian, async (req, res) => {
     // View both account info and library info
     try {
-        const library = await Library.findById(req.user.library); 
+        const library = await Library.findById(req.user.library);
 
-        res.render("librarian/profile", {user: req.user, library: library, errors: []});
-    } catch(e) {
+        res.render("librarian/profile", { user: req.user, library: library, errors: [] });
+    } catch (e) {
         res.status(400).json({ errors: e });
     }
 })
@@ -39,17 +39,17 @@ router.get("/profile", isLibrarian, async (req, res) => {
 router.post("/profile", validateUsername, async (req, res) => {
     // Change account info (only allow changing username)
     try {
-        const library = await Library.findById(req.user.library); 
-        
+        const library = await Library.findById(req.user.library);
+
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.render('librarian/profile', {user: req.user, library: library, errors: errors.array()})
+            return res.render('librarian/profile', { user: req.user, library: library, errors: errors.array() })
         }
-        
-        await Librarian.findByIdAndUpdate(req.user._id, {username: req.body.username});
 
-        res.render("librarian/profile", {user: req.user, library: library, errors: []})
-    } catch(e) {
+        await Librarian.findByIdAndUpdate(req.user._id, { username: req.body.username });
+
+        res.render("librarian/profile", { user: req.user, library: library, errors: [] })
+    } catch (e) {
         res.status(400).json({ errors: e });
     }
 })
@@ -58,52 +58,52 @@ router.post("/profile", validateUsername, async (req, res) => {
 router.get("/inventory", isLibrarian, async (req, res) => {
     // Display all the books
     try {
-        const books = await Book.find({library: req.user.library});
-        res.render("librarian/inventory", {books: books});
-    } catch(e) {
+        const books = await Book.find({ library: req.user.library });
+        res.render("librarian/inventory", { books: books });
+    } catch (e) {
         res.status(400).json({ errors: e });
     }
 })
 
 router.get("/add_book", isLibrarian, (req, res) => {
     // Render form
-    res.render("librarian/add_book", {categories: categoriesArray});
+    res.render("librarian/add_book", { categories: categoriesArray });
 })
 
 router.post("/add_book", upload.array("images", 3), validateBookCreation, async (req, res) => {
     // Confirm form
     if (req.files.length !== 3) {
-        return res.render("librarian/add_book", {errors: [{msg: "Book require exactly 3 images"}], categories: categoriesArray});
+        return res.render("librarian/add_book", { errors: [{ msg: "Book require exactly 3 images" }], categories: categoriesArray });
     }
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.render('librarian/add_book', {errors: errors.array(), categories: categoriesArray  });
+        return res.render('librarian/add_book', { errors: errors.array(), categories: categoriesArray });
     }
 
     try {
         // Category will be a checklist to add multiple tags
-        const {title, author, categories, description, publishDate, amount} = req.body;
+        const { title, author, categories, description, publishDate, amount } = req.body;
 
         // Ensure book is unique within a library
-        const book = await Book.findOne({title: title});
-        if (book) return res.render("librarian/add_book", {errors: [{msg: "Book already exists in this library"}], categories: categoriesArray})
+        const book = await Book.findOne({ title: title });
+        if (book) return res.render("librarian/add_book", { errors: [{ msg: "Book already exists in this library" }], categories: categoriesArray })
 
         // Create a new book
         const newBook = new Book({
-            title: title, 
-            coverImages: req.files.map(file => file.path), 
-            author: author, 
-            category: categories, 
-            description: description, 
-            publishDate: publishDate, 
+            title: title,
+            coverImages: req.files.map(file => file.path),
+            author: author,
+            category: categories,
+            description: description,
+            publishDate: publishDate,
             amount: amount,
             library: req.user.library,
         });
 
         await newBook.save();
         res.redirect("/librarian/inventory");
-    } catch(e) {
+    } catch (e) {
         res.status(400).json({ errors: e });
     }
 })
@@ -114,25 +114,25 @@ router.get("/book_detail/:id", async (req, res) => {
     try {
         const book = await Book.findById(req.params.id);
 
-        res.render("librarian/book_detail", {book: book});
-    } catch(e) {
+        res.render("book/book_detail", { book: book });
+    } catch (e) {
         res.status(400).json({ errors: e });
     }
 })
 
 router.post("/book_detail/:id", isLibrarian, (req, res) => {
     // Change book detail
-    
+
 })
 
 // Pickup and borrow
 router.get("/customer", isLibrarian, async (req, res) => {
     // Render all people who are borrowing the book and book pickups
     try {
-        const borrows = await Borrow.find({library: req.user.library}).populate("reader").populate("book");
-        const pickups = await Pickup.find({library: req.user.library}).populate("reader").populate("books");
-        res.render("librarian/customer", {borrows: borrows, pickups: pickups});
-    } catch(e) {
+        const borrows = await Borrow.find({ library: req.user.library }).populate("reader").populate("book");
+        const pickups = await Pickup.find({ library: req.user.library }).populate("reader").populate("books");
+        res.render("librarian/customer", { borrows: borrows, pickups: pickups });
+    } catch (e) {
         res.status(400).json({ errors: e });
     }
 })
@@ -142,8 +142,8 @@ router.get("/pickup/:id", isLibrarian, async (req, res) => {
     try {
         const pickup = await Pickup.findById(req.params.id).populate("reader").populate("books");
         if (pickup.library !== req.user.library) return res.redirect("/librarian/customer");
-        return res.render("librarian/pickup", {pickup: pickup});
-    } catch(e) {
+        return res.render("librarian/pickup", { pickup: pickup });
+    } catch (e) {
         res.status(400).json({ errors: e });
     }
 })
@@ -151,20 +151,20 @@ router.get("/pickup/:id", isLibrarian, async (req, res) => {
 router.post("/update/pickup/:id", async (req, res) => {
     // confirm, or cancel, or finished
     try {
-        await Pickup.findByIdAndUpdate(req.params.id, {status: req.body.status, takeDate: req.body.takeDate});
+        await Pickup.findByIdAndUpdate(req.params.id, { status: req.body.status, takeDate: req.body.takeDate });
         res.redirect("/librarian/customer");
-    } catch(e) {
+    } catch (e) {
         res.status(400).json({ errors: e });
     }
 })
 
-router.get("/borrow/:id", isLibrarian, async (req, res) =>{
+router.get("/borrow/:id", isLibrarian, async (req, res) => {
     // Borrow detail and change date
     try {
         const borrow = await Borrow.findById(req.params.id).populate("reader").populate("books");
         if (borrow.library !== req.user.library) return res.redirect("/librarian/customer");
-        return res.render("librarian/borrow", {borrow: borrow});
-    } catch(e) {
+        return res.render("librarian/borrow", { borrow: borrow });
+    } catch (e) {
         res.status(400).json({ errors: e });
     }
 })
@@ -172,9 +172,9 @@ router.get("/borrow/:id", isLibrarian, async (req, res) =>{
 router.post("/update/borrow/:id", async (req, res) => {
     // Mark as returned, or canceled
     try {
-        await Borrow.findByIdAndUpdate(req.params.id, {status: req.body.status, dueDate: req.body.dueDate});
+        await Borrow.findByIdAndUpdate(req.params.id, { status: req.body.status, dueDate: req.body.dueDate });
         res.redirect("/librarian/customer");
-    } catch(e) {
+    } catch (e) {
         res.status(400).json({ errors: e });
     }
 })
@@ -192,18 +192,18 @@ router.post("/message/:gmail", isLibrarian, async (req, res) => {
     try {
         transporter.sendMail({
             to: req.params.gmail,
-            subject: `${req.user.username} sent you a message`, 
+            subject: `${req.user.username} sent you a message`,
             html: req.body.message,
         });
 
         // Create a notification
-        const reader = await Reader.findOne({gmail: req.params.gmail});
+        const reader = await Reader.findOne({ gmail: req.params.gmail });
         reader.notification.push({
-            message: req.body.message, 
+            message: req.body.message,
             title: `Librarian ${username} sent you a message`
         })
         await reader.save();
-    } catch(e) {
+    } catch (e) {
         res.status(400).json({ errors: e });
     }
 })
