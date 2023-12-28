31fe358b9a99b88d7efd59c9eb5c3544600e2959
unknown <s3978506@rmit.edu.vn>
2023-12-28 18:29:56 +0700

    handle the error for upload file

--- routes/library.js
+++ routes/library.js
@@ -128,7 +128,10 @@ router.post("/profile", upload.fields([{ name: "logo" }, { name: "banner" }]), v
         if (req.body.username) updateFields.username = req.body.username; 
         if (req.body.description) updateFields.description = req.body.description;
         if (req.files.logo) updateFields.profilePicture = req.files.logo[0].path;
-        if (req.files.banner) updateFields.banner = req.files.banner[0].path;
+        if (req.files.banner) {
+            updateFields.banner = req.files.banner[0].path;
+            updateFields.banner = updateFields.banner.replace(/\\/g, "/"); // This replace will help the frontend display the img (only for this part)
+        }
  
         await Library.findOneAndUpdate({_id: req.user._id}, updateFields); 
         
